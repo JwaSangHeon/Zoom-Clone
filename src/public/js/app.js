c3715815a8396938332fb26e9f9cf599a0f610a1
JwaSangHeon <whk4051@naver.com>
2023-03-03 11:13:51 +0900

    유저의 모든 미디어 접근 및 select 박스를 이용한 카메라 선택

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,11 +1,27 @@
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
 
 let muted = false;
 let cameraOff = false;
 
 let myStream;
+
+async function getCarmera() {
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((device) => device.kind === "videoinput");
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+      camerasSelect.appendChild(option);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
 
 async function getMedia() {
   try {
@@ -14,13 +30,18 @@ async function getMedia() {
       audio: true,
     });
     myFace.srcObject = myStream;
+    await getCarmera();
   } catch (e) {
     console.log(e);
   }
 }
 
 getMedia();
+
 const handleMuteClick = () => {
+  myStream.getAudioTracks().forEach((track) => {
+    track.enabled = !track.enabled;
+  });
   if (!muted) {
     muteBtn.innerText = "UnMute";
     muted = true;
@@ -31,6 +52,9 @@ const handleMuteClick = () => {
 };
 
 const handleCameraClick = () => {
+  myStream.getVideoTracks().forEach((track) => {
+    track.enabled = !track.enabled;
+  });
   if (!cameraOff) {
     cameraBtn.innerText = "Camera Off";
     cameraOff = true;
